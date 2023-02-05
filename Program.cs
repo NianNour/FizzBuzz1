b85ec61a3397c20a820208c7f5cc73b708d76ad6
NianNour <121342724+NianNour@users.noreply.github.com>
2023-02-05 21:46:00 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            int number;
+                Console.WriteLine("Please enter the number.");
+                n = Console.ReadLine();
+                number = Convert.ToInt32(n);
+                for (int i = 1; i <= number; i++)
+                {
+                    if (i % 15 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            
+        }
+    }
+}
